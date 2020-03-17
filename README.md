fb9202a4048d59ea11b03cb98354741ea5497818
dobredani <dobre.dani.gtalk@gmail.com>
2020-03-17 19:45:46 +0200

    LoadView
    Functioneaza deserializarea si view

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # PA-Lab5
+
+Update:
+- am adus completari codului de pe slide-uri ptr a putea compila
+- am completat functia load pentru a citi fisierul .ser si a deserializa obiectul catalog
+- am completat functia view ptr a deschide documentul din catalog
--- src/CatalogUtil.java
+++ src/CatalogUtil.java
@@ -1,22 +1,53 @@
 import javax.print.Doc;
+import javax.sound.midi.Soundbank;
 import java.awt.*;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class CatalogUtil {
     public static void save(Catalog catalog) throws IOException {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(catalog.getPath()))){
             oos.writeObject(catalog);
+
+
         }
     }
 
     public static Catalog load(String path) throws InvalidCatalogException {
+        try {
+            ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(path));
+            Catalog catalog = (Catalog) objectInputStream.readObject();
+
+            objectInputStream.close();
+            return catalog;
+        }
+        catch (IOException ex)
+        {
+            System.out.printf("Cannot open file: " + ex.toString());
+        }
+        catch (ClassNotFoundException ex)
+        {
+            System.out.printf("Invalid data: " + ex.toString());
+        }
+
         return null;
     }
 
     public static void view(Document document){
         Desktop desktop = Desktop.getDesktop();
+
+        try {
+            URI uri = new URI(document.getLocation());
+            desktop.browse(uri);
+        }
+        catch (URISyntaxException ex)
+        {
+            System.out.printf("Invalid URI: " + ex.toString());
+        }
+        catch (IOException ex){
+            System.out.printf("Invalid URI: " + ex.toString());
+        }
 
     }
 }
